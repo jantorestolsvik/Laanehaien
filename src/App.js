d112d7ef822e4b3185fc4483c9f0afbff8fff5a4
jstolsvi <jan.stolsvik@soprasteria.com>
2017-05-09 19:30:01 +0200

    Endret til modular css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { app, appLogo, appHeader, appIntro } from './App.css';
+import classnames from 'classnames';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={classnames(app)}>
+        <div className={classnames(appHeader)}>
+          <img src={logo} className={classnames(appLogo)} alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className={classnames(appIntro)}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
