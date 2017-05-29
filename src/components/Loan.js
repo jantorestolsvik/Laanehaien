4b27e0c1cdffa803c6baea85339df5ab85bae9fc
AndreasDybdahl <adybdahl@one.steria.dom>
2017-05-29 16:01:03 +0200

    Added Risk component

--- src/components/Loan.js
+++ src/components/Loan.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 import styles from './Loan.css';
+import Risk from './Risk.js';
 
 let cx = classnames.bind(styles);
 
@@ -36,7 +37,8 @@ class Loan extends Component {
     const risks = [
       { name: 'Brukket tann', limit: 4999 },
       { name: 'Knekt ben', limit: 1000000 },
-      { name: 'Kneskåler', limit: 3000000 }
+      { name: 'Kneskåler', limit: 3000000 },
+      { name: 'Interne blødninger', limit: 5000000 }
     ]
     return risks.filter((risk) => {
       return risk.limit < loan;
@@ -75,18 +77,7 @@ class Loan extends Component {
           <input style={{ width: 277 }} type="range" name="points" min="0.01" max="12" step="0.2" value={this.state.value} onChange={e => this.handleChange(e.target.value)} />
           <p>Jeg vil låne <span name="amount">{this.renderLoan(parseInt(this.getLoanValue(), 0))},- kr</span></p>
 
-          <div className={classnames(styles.risiko)}>
-            <h3 style={{ margin: 0 }}>Risiko:</h3>
-            <p style={{ marginTop: 0 }}>(Om man ikke betaler)</p>
-
-            <ul style={{ width: 150, margin: '0 auto' }}>
-              {
-                this.getRisks().map((risk, index) => {
-                  return <li key={index}> {risk.name}</li>
-                })
-              }
-            </ul>
-          </div>
+          <Risk risks={this.getRisks()} />
 
           <p>
             <label>
